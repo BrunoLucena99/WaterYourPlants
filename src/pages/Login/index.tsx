73778dec4ceb8a4da621692553e825ac33a0cde3
Bruno <brunohenriquelucena@gmail.com>
2021-08-19 10:37:34 -0300

    feat: create routes and add react-navigation on project

---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+const Login = () => (
+  <SafeAreaView>
+    <Text>Login Page</Text>
+  </SafeAreaView>
+);
+
+export default Login;
---
+++ src/routes/index.tsx
@@ -0,0 +1,18 @@
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import React from 'react';
+import Login from '../pages/Login';
+
+const Routes = () => {
+  const Stack = createNativeStackNavigator();
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={Login} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default Routes;
