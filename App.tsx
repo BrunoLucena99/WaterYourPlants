8429558002ea9464d3aaca9cc9b26915d6b57181
Bruno <brunohenriquelucena@gmail.com>
2021-08-19 12:25:16 -0300

    feat: add transparent statusbar

--- App.tsx
+++ App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import {StatusBar} from 'react-native';
 import {ThemeProvider} from 'styled-components/native';
 import Routes from './src/routes';
 import theme from './src/utils/theme';
 
 const App = () => (
-  <ThemeProvider theme={theme.colors}>
-    <Routes />
-  </ThemeProvider>
+  <>
+    <StatusBar translucent backgroundColor="transparent" />
+    <ThemeProvider theme={theme.colors}>
+      <Routes />
+    </ThemeProvider>
+  </>
 );
 
 export default App;
